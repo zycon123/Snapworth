30f631c7f5517d8f8890a0fbf2cd2384f340bf0c
zycon123 <stian-t-95@hotmail.com>
2026-09-05 02:14:48 +0200

    Delete sw.js

--- sw.js
+++
@@ -1,8 +0,0 @@
-const CACHE='snapworth-v09';
-const ASSETS=['/','/index.html','/manifest.webmanifest','/icon-192.png','/icon-512.png'];
-self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
-self.addEventListener('activate',e=>e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k))))));
-self.addEventListener('fetch',e=>{
-  if(e.request.method!=='GET') return;
-  e.respondWith(fetch(e.request).catch(()=>caches.match(e.request)));
-});
